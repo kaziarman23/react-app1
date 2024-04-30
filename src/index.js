ef817e1e01b941f56445133432f28be3e4943c18
kaziarman23 <kaziarman@proton.me>
2024-04-30 18:08:48 +0600

    problem resoled

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDom from "react-dom/client"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "../src/style.css"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const img = "https://images.unsplash.com/photo-1712650827891-cf87e3b8aaec?q=80&w=1539&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+const title = "Intersting Facts For Curious Minds";
+const author = "Jordan Moore";
+
+const BookList = () => {
+    return (
+        <section className="section">
+            <Book author={author} title={title} img={img}/>
+            <Book author={author} title={title} img={img}/>
+        </section>
+    );
+};
+
+const Book = (props) => {
+    return (
+        <article className="article">
+            <img src={props.img} alt={props.title} />
+            <h2>{props.title}</h2>
+            <h4>{props.author}</h4>            
+        </article>
+    );
+};
+
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+
+root.render(<BookList/>);
+
+
+// git push : 2;
